8fecfd2920ed3f53aa1f61e9c9c0370fa0a5ce2c
Yamini Settipalli <139889042+yamini749@users.noreply.github.com>
2024-09-09 11:13:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let userEntries = [];
+
+const saveUserForm = (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptedTermsAndConditions = document.getElementById("acceptTerms").checked;
+
+    const entry = {
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndConditions
+    };
+
+    userEntries.push(entry);
+    localStorage.setItem("user-entries", JSON.stringify(userEntries));
+};
+
+userForm.addEventListener("submit", saveUserForm);
